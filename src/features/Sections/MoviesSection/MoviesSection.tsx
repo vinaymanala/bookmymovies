ffc2194700468cb147b4e42dae53049fddad98da
manalavi <vinay.manala@outlook.com>
2023-11-05 17:16:33 +0530

    [RF] Remove unused state

--- src/features/Sections/MoviesSection/MoviesSection.tsx
+++ src/features/Sections/MoviesSection/MoviesSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import "./index.css";
 import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../../utils/hooks";
--- src/features/Sections/PopularMoviesSection/PopularMoviesSection.tsx
+++ src/features/Sections/PopularMoviesSection/PopularMoviesSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./index.css";
 import { useAppDispatch, useAppSelector } from "../../../utils/hooks";
 import { getPopularMoviesByIndex } from "./PopularMoviesSlice";
